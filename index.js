90d94d277444b2caae7b26bacaf0fb0edcd6090a
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-24 00:15:32 +0600

    Used Cors.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', async (req, res) => {
 	res.send('The Server is running.');
