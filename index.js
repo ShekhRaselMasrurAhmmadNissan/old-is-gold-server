e100a2f12f9a44deb6f6196b851ae5d803bb757d
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-26 11:03:08 +0600

    Created Users collection and POSt User api.

--- index.js
+++ index.js
@@ -8,9 +8,48 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-	res.send('The Server is running.');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
 });
+
+const run = async () => {
+	const UsersCollection = client.db('Old-Is-Gold').collection('Users');
+
+	app.get('/', async (req, res) => {
+		res.send('The Server is running.');
+	});
+
+	/**
+	 * Users Operations Start
+	 */
+
+	// Get All Users
+	app.get('/users', async (req, res) => {
+		const query = {};
+		const users = await UsersCollection.find(query).toArray();
+		res.send(users);
+	});
+
+	// Post a User
+	app.post('/users', async (req, res) => {
+		const user = req.body;
+		const result = await UsersCollection.insertOne(user);
+		res.send(result);
+	});
+
+	/**
+	 * Users Operation Finished
+	 */
+};
+run()
+	.then(() => {})
+	.catch((err) => {
+		console.error(err.name, err.message, err.stack);
+	});
 
 app.listen(port, () => {
 	console.log(`The server is running at port: ${port}`);
