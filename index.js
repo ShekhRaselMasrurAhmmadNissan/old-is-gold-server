dc06218681f97fb358d70ec98a4047784e222153
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-26 11:06:29 +0600

    Generate Token.

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ const run = async () => {
 	});
 
 	/**
+	 * Authentications Start
+	 */
+	// JWT Token
+	app.get('/jwt', async (req, res) => {
+		const email = req.query.email;
+		const query = { email: email };
+		const user = await UsersCollection.findOne(query);
+		console.log(user);
+		if (user) {
+			const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: '10d',
+			});
+			return res.send({ accessToken: token });
+		}
+		res.status(403).send({ accessToken: '' });
+	});
+	/**
+	 * Authentications End
+	 */
+
+	/**
 	 * Users Operations Start
 	 */
 
